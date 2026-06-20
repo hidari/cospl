e3a327d0c81057d4dbcb9c3533861eb085958578
Hidari <ika5140@gmail.com>
2026-06-21 04:26:49 +0900

    test: E2E シナリオ コピーのフラッシュ通知を追加

---
+++ e2e/copy.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "./fixtures";
+
+test("コピーを押すと aria-live のフラッシュに『コピーしました』が出る", async ({
+  licensePage,
+}) => {
+  await licensePage.goto();
+  await licensePage.copyButton.click();
+  // フラッシュは 1400ms で復帰するが、assertion は即時ポーリングで一致を捉える。
+  await expect(licensePage.flashStatus).toHaveText("コピーしました");
+});
